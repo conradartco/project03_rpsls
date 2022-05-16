8333e2185a46c513501def99f1df26d978779c81
Jonathan Conrad <jc@Jonathans-MBP.home>
2022-05-16 12:10:33 -0700

    added file folders to remote repository

---
+++ main.py
@@ -0,0 +1,33 @@
+# make at least 10 commits
+
+# properly incorporate inheritance into my game
+
+# manage bad user input, reobtain input if necessary
+
+# store all gesture options in a list. Utilize this list within my code
+# (display gesture options, assign player a gesture, etc)
+
+# correct player should win based on choices made by player
+
+# game of RSPSLS should be best of 3 to decide winner
+
+# create the option for human vs. human and human vs. ai
+
+# ---
+
+# Classes
+# - Player(parent)
+#   Human(child)
+#   AI(child)
+# - Game (runs the game process)
+
+# Write an algorithm that represents steps necessaary to play in a best of 3 format
+# 1. Welcome message
+# 2. Display rules of the game
+# 3. Ask how many players will be playing (will determine human player or ai player)
+# 4. Opponents choose their gestures
+# 5. Compare gestures
+# 6. Determine winner of that round (or tie)
+# 7. Check to see if anyone has won the game
+#    If someone has NOT won the game, repeat steps 4-7
+#    If someone HAS won the game, end game and display winner
